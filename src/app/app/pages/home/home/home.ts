d2dac44bff756fbad74d297e0ed7dea2cac1a6cf
Emad Heiba <EMADHEBA975@GMAIL.COM>
2026-06-08 09:02:53 +0300

    Apply design changes and fix redirects

--- src/app/app/pages/home/home/home.ts
+++ src/app/app/pages/home/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
